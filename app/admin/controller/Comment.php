dcf005c146919046c08e693850d1bcda67d6f612
guoguo <2354687725@qq.com>
2021-02-15 14:10:18 +0800

    补上后台评论功能

--- app/admin/controller/Comment.php
+++ app/admin/controller/Comment.php
@@ -3,7 +3,9 @@
 
 namespace app\admin\controller;
 
+use app\model\Book;
 use app\model\Comments;
+use app\model\User;
 use think\db\exception\DataNotFoundException;
 use think\db\exception\ModelNotFoundException;
 use think\facade\View;
@@ -12,52 +14,58 @@ class Comment extends BaseAdmin
 {
     public function index()
     {
-        $map = array();
-        $uid = input('uid');
-        if ($uid){
-            $map[] = ['user_id','=',$uid];
-        }
-
-        $book_id = input('book_id');
-        if ($book_id){
-            $map[] = ['book_id','=',$book_id];
-        }
-        $data = Comments::with(['book','user'])->where($map);
-        $comments = $data->order('id', 'desc')->paginate(
-            [
-                'list_rows'=> 5,
-                'query' => request()->param(),
-                'var_page' => 'page',
-            ]);
-//        ->each(function ($item, $key) {
-//            $dir = Env::get('root_path') . '/public/static/upload/comments/' . $item->book->id . '/';
-//            $item['content'] = file_get_contents($dir . $item->id . '.txt'); //获取用户评论内容
-//        });
-        View::assign([
-            'comments' => $comments,
-            'count' => $data->count()
-        ]);
         return view();
     }
 
-    public function delete(){
+    public function list()
+    {
+        $page = intval(input('page'));
+        $limit = intval(input('limit'));
+        $map = array();
+        $uid = input('uid');
+        if ($uid) {
+            $map[] = ['user_id', '=', $uid];
+        }
+
+        $book_id = input('book_id');
+        if ($book_id) {
+            $map[] = ['book_id', '=', $book_id];
+        }
+        $data = Comments::where($map);
+        $count = $data->count();
+        $comments = $data->order('id', 'desc')
+            ->limit(($page - 1) * $limit, $limit)->select();
+        foreach ($comments as &$comment)
+        {
+            $comment['user'] = User::where('user_id','=',$comment['user_id'])->column('username');
+            $comment['book'] = Book::where('book_id','=',$comment['book_id'])->column('book_name');
+        }
+        return json([
+            'code' => 0,
+            'msg' => '',
+            'count' => $count,
+            'data' => $comments
+        ]);
+    }
+
+    public function delete()
+    {
         $id = input('id');
         try {
             $comment = Comments::findOrFail($id);
             $result = $comment->delete();
             if ($result) {
-                return ['err' => '0','msg' => '删除成功'];
+                return ['err' => '0', 'msg' => '删除成功'];
             } else {
-                return ['err' => '1','msg' => '删除失败'];
+                return ['err' => '1', 'msg' => '删除失败'];
             }
-        } catch (DataNotFoundException $e) {
-            abort(404, $e->getMessage());
         } catch (ModelNotFoundException $e) {
-            abort(404, $e->getMessage());
+            return ['err' => '1', 'msg' => $e->getMessage()];
         }
     }
 
-    public function deleteAll($ids){
+    public function deleteAll($ids)
+    {
         Comments::destroy($ids);
     }
 }
