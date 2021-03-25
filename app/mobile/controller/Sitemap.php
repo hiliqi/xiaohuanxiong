f8cf95b241b150d0992b8d02fc575d811778261a
guoguo <2354687725@qq.com>
2021-03-25 14:47:45 +0800

    增加动态地图

---
+++ app/mobile/controller/Sitemap.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace app\mobile\controller;
+
+
+use app\model\Book;
+use app\model\Chapter;
+
+class Sitemap extends Base
+{
+    public function book()
+    {
+        $num = config('seo.sitemap_gen_num');
+        $site_name = config('site.mobile_domain');
+        $books = Book::order('id','desc')->limit($num)->select();
+        $data = array();
+        $content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+        $content .= '<urlset>';
+        foreach ($books  as &$book){ //这里构建所有的内容页数组
+            if ($this->end_point == 'id') {
+                $book['param'] = $book['id'];
+            } else {
+                $book['param'] = $book['unique_id'];
+            }
+            $temp = array(
+                'loc' => $site_name . '/m/' . BOOKCTRL . '/' . $book['param'],
+                'priority' => '0.9',
+            );
+            array_push($data, $temp);
+        }
+        foreach ($data as $item) {
+            $content .= $this->create_item($item);
+        }
+        $content .= '</urlset>';
+
+        ob_clean();
+        return xml($content,200,[],['root_node'=>'xml']);
+    }
+
+    public function chapter() {
+        $num = config('seo.sitemap_gen_num');
+        $site_name = config('site.domain');
+        $chapters = Chapter::order('id','desc')->limit($num)->select();
+        $arr = array();
+        $content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+        $content .= '<urlset>';
+        foreach ($chapters as $chapter) {
+            $temp = array(
+                'loc' => $site_name . '/m/' . CHAPTERCTRL . '/' . $chapter['id'],
+                'priority' => '0.9',
+            );
+            array_push($arr, $temp);
+        }
+        foreach ($arr as $item) {
+            $content .= $this->create_item($item);
+        }
+        $content .= '</urlset>';
+        ob_clean();
+        return xml($content,200,[],['root_node'=>'xml']);
+    }
+
+    private function create_item($data)
+    {
+        $item = "<url>";
+        $item .= "<loc>" . $data['loc'] . "</loc>";
+        $item .= "<priority>" . $data['priority'] . "</priority>";
+        $item .= "</url>";
+        return $item;
+    }
+}
