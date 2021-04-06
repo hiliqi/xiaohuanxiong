2f5f9b6c11f4d1dc1a26dbc94ada0fa374e63f91
guoguo <2354687725@qq.com>
2021-04-07 00:48:58 +0800

    1

--- app/app/controller/Account.php
+++ app/app/controller/Account.php
@@ -151,6 +151,30 @@ class Account extends Base
             } else {
                 return json(['success' => 0, 'msg' => '登录失败，请尝试重新登录']);
             }
+        } catch (DataNotFoundException $e) {
+            $user = new User();
+            $user->username = gen_uid(12);
+            $user->password = 'abc123';
+            $user->suid = $data['suid'];
+            $user->level = 1; //游客
+            $user->mobile = '';
+            $result = $user->save();
+            if ($result) {
+                $token = [
+                    "iat" => time(), //签发时间
+                    "nbf" => time(), //在什么时候jwt开始生效  （这里表示生成100秒后才生效）
+                    "exp" => time() + 60 * 60 * 24, //token 过期时间
+                    "uid" => $user->id, //记录的userid的信息，这里是自已添加上去的，如果有其它信息，可以再添加数组的键值对
+                    "suid" => $user->suid,
+                    "level" => $user->level
+                ];
+                $utoken = JWT::encode($token, $key, "HS256");
+                $user['utoken'] = $utoken;
+                $user['balance'] = 0;
+                return json(['success' => 1, 'userInfo' => $user]);
+            } else {
+                return json(['success' => 0, 'msg' => '登录失败，请尝试重新登录']);
+            }
         }
     }
 
